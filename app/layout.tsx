b03daac12cc8c05a74ce24a04db3d26fea4850f9
donghwan <runiarang@gmail.com>
2025-07-09 14:15:56 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'tetris App',
   description: 'Created with v0',
-  generator: 'v0.dev',
+  generator: 'tetris.dev',
 }
 
 export default function RootLayout({
